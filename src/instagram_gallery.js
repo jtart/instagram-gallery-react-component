e676be95dfd25f88e2465b0f117a41ac353f1c2b
Jordan Tart <jordan.tart3@gmail.com>
2016-09-24 23:58:05 +0100

    Static rendering of gallery

---
+++ src/instagram_gallery.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import InstagramGallery from './containers/InstagramGallery'
+
+ReactDOM.render(
+  <InstagramGallery />,
+  document.getElementById('instagram-gallery-container')
+)
