cdc2ad9ca22c7ac5ecaad95393ce1a63994f347f
Jordan Tart <jordan.tart3@gmail.com>
2016-09-25 18:24:16 +0100

    Added gallery container

---
+++ src/containers/InstagramGallery.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const InstagramGallery = React.createClass({
+  render () {
+    return (
+      <div>
+        'Hello world'
+      </div>
+    )
+  }
+})
+
+export default InstagramGallery
